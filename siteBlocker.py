d4df057829b86871b7a7c6972a8f0166ab88dfbe
xalxnder <xalexander51@gmail.com>
2020-04-20 06:44:21 -0400

    Refactored, and added functions to a class.

--- siteBlocker.py
+++ siteBlocker.py
@@ -11,49 +11,56 @@ elevate(graphical=False)
 user_input = input(
     "Would you like to add or remove a site from your host file? ")
 
+site_name = input("What is the name of the site ")
+
+
+class SiteBlocker:
+    """ A model for our site blocker program """
+
+    def __init__(self):
+        self.user_input = user_input
 
 # Function to add site from host file.
-def add_site():
-    # Get input from user.
-    user_input_add = input("What site would you like to block?: ")
-    # Open host file, and check to see if string is already there.
-    with open('/etc/hosts', 'r') as file:
-        lines = file.read()
-        result = re.search(rf'\b{user_input_add}\b', lines)
-        while result:
+    def add(self):
+        # Open host file, and check to see if string is already there.
+        with open('/etc/hosts', 'r') as file:
+            lines = file.read()
+            # Checking to find anything that matches site_name
+            result = re.search(rf'\b{site_name}\b', lines)
+            while result:
+                # Set Reference point to beginning of file
+                file.seek(0)
+                used_site_name = input(
+                    "Site is already being blocked. What site would you like to block?: ")
+                result = re.search(rf'\b{used_site_name}\b', lines)
+            else:
+                with open('/etc/hosts', 'a+') as file:
+                    file.write(
+                        f"\n127.0.0.1\t{site_name}.com\n127.0.0.1\twww.{site_name}.com\n127.0.0.1\thttps://www.{site_name}.com")
+
+    # Function to remove site from host file.
+    def remove(self):
+        with open('/etc/hosts', 'r+') as file:
+            new_output = []
+            # pattern = re.compile(r'\bworldstar\b')
+            lines = file.readlines()
+            for i in lines:
+                new_output.append(i)
+                if site_name in i:
+                    new_output.remove(i)
+                print(i)
             file.seek(0)
-            user_input_add = input(
-                "Site is already being blocked. What site would you like to block?: ")
-            result = re.search(rf'\b{user_input_add}\b', lines)
-        else:
-            with open('/etc/hosts', 'a+') as file:
-                file.write(
-                    f"\n127.0.0.1\t{user_input_add}.com\n127.0.0.1\twww.{user_input_add}.com\n127.0.0.1\thttps://www.{user_input_add}.com")
-
-# Function to remove site from host file.
+            file.writelines(new_output)
+            file.truncate()
+        with open('/etc/hosts', 'r') as file:
+            print(file.read())
 
 
-def remove_site():
-    user_input_remove = input("What site would you like to remove? ")
-    with open('/etc/hosts', 'r+') as file:
-        new_output = []
-        # pattern = re.compile(r'\bworldstar\b')
-        lines = file.readlines()
-        for i in lines:
-            new_output.append(i)
-            if user_input_remove in i:
-                new_output.remove(i)
-            print(i)
-        file.seek(0)
-        file.writelines(new_output)
-        file.truncate()
-    with open('/etc/hosts', 'r') as file:
-        print(file.read())
-
+site_blocker = SiteBlocker()
 
 if user_input == 'add':
-    add_site()
+    site_blocker.add()
 elif user_input == 'remove':
-    remove_site()
+    site_blocker.remove()
 else:
     print('You did not make an approptiate choice. Exiting..')
